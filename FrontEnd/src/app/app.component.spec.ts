bd6dc71da54fd56e0a52b813a96d7773c937e736
Otire82 <a.jeanne01@sncf.fr>
2022-11-21 15:41:49 +0100

    initialize mvc spring angular

--- FrontEnd/src/app/app.component.spec.ts
+++ FrontEnd/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'project_4'`, () => {
+  it(`should have as title 'angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('project_4');
+    expect(app.title).toEqual('angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project_4 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular app is running!');
   });
 });
