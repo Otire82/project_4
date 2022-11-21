3d4bf4cc5f810ddc641620abab433b3468ad014f
Otire82 <a.jeanne01@sncf.fr>
2022-11-21 21:24:26 +0100

    back initialize

---
+++ BackEnd/movieselct/src/main/java/fr/codewise/movieselct/MovieselctApplication.java
@@ -0,0 +1,13 @@
+package fr.codewise.movieselct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieselctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieselctApplication.class, args);
+	}
+
+}
---
+++ BackEnd/movieselct/src/test/java/fr/codewise/movieselct/MovieselctApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.codewise.movieselct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieselctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
