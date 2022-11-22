0edd3985e6a300355b0883728041f047d7f1fbe8
Otire82 <a.jeanne01@sncf.fr>
2022-11-22 18:15:00 +0100

    debug post with postman

--- BackEnd/movieselect/src/main/java/fr/codewise/movieselect/entities/Movie.java
+++ BackEnd/movieselect/src/main/java/fr/codewise/movieselect/entities/Movie.java
@@ -37,9 +37,9 @@ public class Movie {
         // this.setIsBorrow(isBorrow);
     }
 
-    public int getId() {return id;}
+    public Integer getId() {return id;}
 
-    public void setId(int id) { this.id = id; }
+    public void setId(Integer id) { this.id = id; }
 
     public String getTitle() { return title; }
 
