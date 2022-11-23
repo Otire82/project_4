03fbabf0d5dead85e35cc88dfa9e51e572c74baa
Otire82 <a.jeanne01@sncf.fr>
2022-11-23 09:34:04 +0100

    modified entity

--- BackEnd/movieselect/src/main/java/fr/codewise/movieselect/entities/Movie.java
+++ BackEnd/movieselect/src/main/java/fr/codewise/movieselect/entities/Movie.java
@@ -38,7 +38,7 @@ public class Movie {
     }
 
     public Integer getId() {return id;}
-
+    
     public void setId(Integer id) { this.id = id; }
 
     public String getTitle() { return title; }
