b80004724f4691edcf4b3cc2ea9dfbb1424ce67d
Otire82 <a.jeanne01@sncf.fr>
2022-11-24 15:58:16 +0100

    movie css

--- FrontEnd/src/app/movie-page/movie-page.component.ts
+++ FrontEnd/src/app/movie-page/movie-page.component.ts
@@ -6,24 +6,26 @@ import { MovieService } from '../movie.service';
 @Component({
   selector: 'app-movie-page',
   templateUrl: './movie-page.component.html',
-  styleUrls: ['./movie-page.component.css']
+  styleUrls: ['./movie-page.component.css'],
 })
 export class MoviePageComponent implements OnInit {
-id!: number;
-movie!: Movie;
- 
-  constructor(private route: ActivatedRoute, private router: Router ,public movieService: MovieService) { }
+  id!: number;
+  movie!: Movie;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    public movieService: MovieService
+  ) {}
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params['id'];
 
     this.movieService.findMovie(this.id).subscribe((movie: Movie) => {
       this.movie = movie;
-    })
+    });
   }
-    list(){
-      this.router.navigate(['movies']);
-    }
+  list() {
+    this.router.navigate(['movies']);
   }
-
-
+}
